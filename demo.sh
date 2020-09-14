3df69ade5663cdc8cbbd818cc3901a73dc641ddc
raghulnarayanasami <58605993+raghulnarayanasami@users.noreply.github.com>
2020-09-14 07:20:09 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /root
 source env/bin/activate
-cd /root/demoproject
 kill -9 $(lsof -ti :8001)
 nohup python manage.py runserver 0.0.0.0:8001 >> /root/demoproject/event.log &
 echo 'Done'
